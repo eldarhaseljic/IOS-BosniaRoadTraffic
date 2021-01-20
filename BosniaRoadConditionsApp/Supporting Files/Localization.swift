9cb0e409a7b65ecc4edef810d4d2825ada048fb0
Eldar Haseljic <eldar.haseljic@virginpulse.com>
2021-01-20 21:18:01 +0100

    language support added

---
+++ BosniaRoadConditionsApp/Supporting Files/Localization.swift
@@ -0,0 +1,10 @@
+//
+//  Localization.swift
+//  BosniaRoadConditionsApp
+//
+//  Created by Eldar Haseljic on 1/20/21.
+//
+
+import Foundation
+
+let BOSNIA_ROAD_CONDITIONS: String = NSLocalizedString("bosnia_road_conditions", comment: "Bosnia road conditions")
